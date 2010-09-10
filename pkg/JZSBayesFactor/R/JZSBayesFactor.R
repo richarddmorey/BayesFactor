c00f481fab525c2c7005c98bd3d2feab4c3e6e53
Richard D. Morey <richarddmorey@gmail.com>
2010-09-10 22:24:31 +0000

    Working JZS Bayes factor in c.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesfactorpcl@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/JZSBayesFactor/R/JZSBayesFactor.R
@@ -0,0 +1,49 @@
+JZSgibbs = function(y,iterations=1000,rscale=1,progress=TRUE){
+	N = as.integer(length(y))
+	iterations = as.integer(iterations)
+	if(progress){
+		progress = round(iterations/100)
+		pb = txtProgressBar(min = 0, max = as.integer(iterations), style = 3) 
+	}else{ 
+		pb=NULL 
+	}
+
+    pbFun = function(samps){ if(progress) setTxtProgressBar(pb, samps)}
+	
+	chains = .Call("RgibbsOneSample", y, N, rscale, iterations,
+				progress, pbFun, new.env(), package="JZSBayesFactor")
+
+	rownames(chains) = c("mu","sig2","g","delta","CMDE")			
+	return(mcmc(t(chains)))
+}
+
+dinvgamma = function (x, shape, scale = 1) 
+{
+    if (shape <= 0 | scale <= 0) {
+        stop("Shape or scale parameter negative in dinvgamma().\n")
+    }
+    alpha <- shape
+    beta <- scale
+    log.density <- alpha * log(beta) - lgamma(alpha) - (alpha + 
+        1) * log(x) - (beta/x)
+    return(exp(log.density))
+}
+
+t.joint=function(g,t,n,nu,r2)
+{
+t1=-.5*log(1+n*g*r2)
+t2=(-(nu+1)/2)*log(1+t^2/((1+n*g*r2)*(nu)))
+return(dinvgamma(g,.5,.5)*exp(t1+t2))
+}
+
+t.value.bf=function(t,n1,n2=0,sd=1,prior.cauchy=T)
+{
+nu=ifelse(n2==0 | is.null(n2),n1-1,n1+n2-2)
+n=ifelse(n2==0 | is.null(n2),n1,(n1*n2)/(n1+n2))
+r2=sd*sd
+marg.like.0=(1+t^2/(nu))^(-(nu+1)/2)
+marg.like.1=ifelse(prior.cauchy,
+  integrate(t.joint,lower=0,upper=Inf,t=t,n=n,nu=nu,r2=r2)$value,
+  (1+n*r2)^(-.5)*(1+t^2/((1+n*r2)*(nu)))^(-(nu+1)/2))
+return(marg.like.0/marg.like.1)
+}
--- pkg/JZSBayesFactor/src/JZSBayesFactor.c
+++ pkg/JZSBayesFactor/src/JZSBayesFactor.c
@@ -11,32 +11,43 @@
 #include <R_ext/Constants.h>
 #include <R_ext/Random.h>
 
+void gibbsOneSample(double *y, int N, double rscale, int iterations, double *chains, int progress, SEXP pBar, SEXP rho);
 
-
-SEXP RgibbsOneSample(SEXP yR, SEXP NR, SEXP rscaleR, SEXP iterationsR)
+SEXP RgibbsOneSample(SEXP yR, SEXP NR, SEXP rscaleR, SEXP iterationsR, SEXP progressR, SEXP pBar, SEXP rho)
 {
 	
-	int npars = 3,iterations = INTEGER_VALUE(iterationsR);
-	int N = INTEGER_VALUE(NR);
+	int npars = 5,iterations = INTEGER_VALUE(iterationsR);
+	int N = INTEGER_VALUE(NR), progress = INTEGER_VALUE(progressR);
 	double rscale = NUMERIC_VALUE(rscaleR);
 	double *y = REAL(yR);
+	
 	
 	SEXP chainsR;
 	PROTECT(chainsR = allocMatrix(REALSXP, npars, iterations));
 
-	gibbsOneSample(y, N, rscale, iterations, REAL(chainsR));
+	gibbsOneSample(y, N, rscale, iterations, REAL(chainsR), progress, pBar, rho);
+	
+	UNPROTECT(1);
 	
 	return(chainsR);
 }
 
-void gibbsOneSample(double *y, int N, double rscale, int iterations, double *chains)
+void gibbsOneSample(double *y, int N, double rscale, int iterations, double *chains, int progress, SEXP pBar, SEXP rho)
 {
 	int i=0;
-	double yBar=0,sumy2=0,rscaleSq=pow(rscale,2);
+	double yBar=0,sumy2=0,rscaleSq=pow(rscale,2),densDelta=0,meanDelta=0,varDelta=1;
 	double mu=0,sig2=1,g=1;
 	double meanMu=0, varMu=0, shapeSig2=(1.0*N)/2+0.5,scaleSig2=1,scaleg=1;
-	int npars = 3;
+	int npars = 5;
 	
+	// progress stuff
+	SEXP sampCounter, R_fcall;
+	int *pSampCounter;
+    PROTECT(R_fcall = lang2(pBar, R_NilValue));
+	PROTECT(sampCounter = NEW_INTEGER(1));
+	pSampCounter = INTEGER_POINTER(sampCounter);
+	
+	GetRNGstate();
 	
 	for(i=0;i<N;i++)
 	{
@@ -47,23 +58,43 @@ void gibbsOneSample(double *y, int N, double rscale, int iterations, double *cha
 	// start MCMC
 	for(i=0; i<iterations; i++)
 	{
+		R_CheckUserInterrupt();
+	
 		
+		//Check progress
+		if(progress && !((i+1)%progress)){
+			pSampCounter[0]=i+1;
+			SETCADR(R_fcall, sampCounter);
+			eval(R_fcall, rho); //Update the progress bar
+		}
+	
+	
 		// sample mu
 		meanMu = yBar * N/(1.0*N+1/g);
-		varMu  = sig2/(1.0*N + 1/g);
+		meanDelta = meanMu/sqrt(sig2);
+		varDelta = 1/(1.0*N + 1/g);
+		varMu  = sig2*varDelta;
 		mu = rnorm(meanMu,sqrt(varMu));
+		densDelta = dnorm(0,meanDelta,sqrt(varDelta),0);
 		
 		// sample sig2
-		scaleSig2 = 0.5*(sumy2 - 2.0 * N * yBar + 1.0*N*pow(mu,2));
+		scaleSig2 = 0.5*(sumy2 - 2.0 * N * yBar * mu + 1.0*N*pow(mu,2));
 		sig2 = 1/rgamma(shapeSig2,1/scaleSig2);
-		
+	
 		// sample g
 		scaleg = 0.5 * (pow(mu,2)/sig2+rscaleSq);
 		g = 1/rgamma(1,1/scaleg);
 		
+		
 		chains[npars*i + 0] = mu;
 		chains[npars*i + 1] = sig2;
 		chains[npars*i + 2] = g;
+		chains[npars*i + 3] = mu/sqrt(sig2);
+		chains[npars*i + 4] = densDelta;
+		
 	}
 
+	UNPROTECT(2);
+	PutRNGstate();
+	
 }
