b87b9d293fd7a64e455f51bdda1bac7809a8afb5
Richard D. Morey <richarddmorey@gmail.com>
2012-10-11 09:23:44 +0000

    Added a "BFInfo()" function that will contain the date, version, etc.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesfactorpcl@153 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/BayesFactorPCL/R/version.R
@@ -0,0 +1,13 @@
+BFInfo <- function()
+{
+	print("Package BayesFactor")
+	print(packageDescription("BayesFactor")$Version)
+  myRev <- '$Rev$'
+  myDate <- '$Date$'
+  nRev <- nchar(myRev)
+  nDate <- nchar(myDate)
+  myRev <- substr(myRev,2,nRev-1)
+  myDate <- substr(myDate,2,nDate-1)
+  print(myRev)
+	print(myDate)
+}
