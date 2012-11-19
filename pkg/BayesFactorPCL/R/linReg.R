326bf02fab7a0cd388821b583c293cb60cdd288a
Richard D. Morey <richarddmorey@gmail.com>
2012-11-19 15:41:19 +0000

    Added allLinearReg() to compute all linear regression models
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesfactorpcl@194 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BayesFactorPCL/R/linReg.R
+++ pkg/BayesFactorPCL/R/linReg.R
@@ -1,5 +1,5 @@
-linearReg.Quad=function(N,p,R2,logbf=FALSE) {
-	h=integrate(integrand.regression,lower=0,upper=Inf,N=N,p=p,R2=R2)
+linearReg.Quad=function(N,p,R2,rscale=1,logbf=FALSE) {
+	h=integrate(integrand.regression,lower=0,upper=Inf,N=N,p=p,R2=R2,rscaleSqr=rscale^2)
 	if(logbf){
 		return(log(h$value))
 	}else{
@@ -8,8 +8,8 @@ linearReg.Quad=function(N,p,R2,logbf=FALSE) {
 }
 
 
-integrand.regression=function(g,N,p,R2)
+integrand.regression=function(g,N,p,R2,rscaleSqr=1)
 {
        a=.5*((N-p-1)*log(1+g)-(N-1)*log(1+g*(1-R2)))
-       exp(a)*dinvgamma(g,shape=.5,scale=N/2)
+       exp(a)*dinvgamma(g,shape=.5,scale=rscaleSqr*N/2)
 }
