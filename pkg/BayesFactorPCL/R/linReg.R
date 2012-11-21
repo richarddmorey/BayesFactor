edff867952d51dea69d39835af2319c9ff92ec86
Richard D. Morey <richarddmorey@gmail.com>
2012-11-21 17:31:19 +0000

    added Gibbs sampler to the regression functions. Removed EqVariance functions. Incremented version number.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesfactorpcl@196 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BayesFactorPCL/R/linReg.R
+++ pkg/BayesFactorPCL/R/linReg.R
@@ -13,3 +13,62 @@ integrand.regression=function(g,N,p,R2,rscaleSqr=1)
        a=.5*((N-p-1)*log(1+g)-(N-1)*log(1+g*(1-R2)))
        exp(a)*dinvgamma(g,shape=.5,scale=rscaleSqr*N/2)
 }
+
+linearReg.Gibbs <- function(y, covariates, iterations = 10000, rscale = 1, progress = TRUE, gibi=NULL){
+  	X <- apply(covariates,2,function(v) v - mean(v))
+  	y = matrix(y,ncol=1)
+  	N = length(y)
+  	p = ncol(X)
+  	Cn = diag(N) - matrix(1,N,N)/N 
+  	XtX = t(X)%*%X
+  	XtCnX = t(X)%*%Cn%*%X
+  	XtCny = t(X)%*%Cn%*%y
+  	Cny = Cn%*%y
+
+    sig2start = sum( (X%*%solve(XtCnX)%*%XtCny - Cny)^2 ) / N
+      
+	if(!is.null(gibi)) {
+		progress=TRUE;
+		if(!is.function(gibi))
+			stop("Malformed GIBI argument (not a function). You should not set this argument if running oneWayAOV.Gibbs from the console.")
+	}
+	if(progress & is.null(gibi)){
+		pb = txtProgressBar(min = 0, max = 100, style = 3) 
+	}else{ 
+		pb=NULL 
+	}
+	
+    pbFun = function(samps){ 
+    	if(progress){
+    		percent = as.integer(round(samps / iterations * 100))
+    		if(is.null(gibi)){
+    			setTxtProgressBar(pb, percent)
+    		}else{
+    			gibi(percent)
+    		}
+    	}
+    }
+
+	chains = .Call("RGibbsLinearReg", 
+						 as.integer(iterations), 
+						 Cny,
+						 X,
+						 XtX,
+						 XtCnX,
+						 XtCny,
+						 as.integer(N),
+						 as.integer(p),
+						 rscale,
+             sig2start,
+						 progress,
+						 pbFun,
+						 new.env(),
+						 package="BayesFactor")
+						 
+  if(progress & is.null(gibi)) close(pb);
+  chains = t(chains)
+  
+  colnames(chains) = c(paste("beta",1:p,sep=""),"sig2","g")
+  return(mcmc(chains))
+
+}
--- pkg/BayesFactorPCL/src/BFPCL.h
+++ pkg/BayesFactorPCL/src/BFPCL.h
@@ -15,40 +15,9 @@
 
 void gibbsOneSample(double *y, int N, double rscale, int iterations, double *chains, int doInterval, double *interval, int progress, SEXP pBar, SEXP rho);
 
-void gibbsEqVariance(double *y, int *N, int J, int I, double lambda, int iterations, double *chains, double sdMetropSig2, double sdMetropTau, int progress, SEXP pBar, SEXP rho);
-
 void gibbsOneWayAnova(double *y, int *N, int J, int sumN, int *whichJ, double rscale, int iterations, double *chains, double *CMDE, SEXP debug, int progress, SEXP pBar, SEXP rho);
 
 double sampleSig2EqVar(double sig2, double *mu, double tau, double *yBar, double *SS, int *N, int sumN, int J, double sdMetrop, double *acc);
-
-double logFullCondTauEqVar(double tau, double *mu, double sig2, double *yBar, double *SS, int *N, int J, double lambda);
-
-double logFullCondSig2EqVar(double sig2, double *mu, double tau, double *yBar, double *SS, int *N, int sumN, int J);
-
-double sampleTauEqVar(double tau, double *mu, double sig2, double *yBar, double *SS, int *N, int J, double lambda, double sdMetrop, double *acc);
-
-void gibbsEqVarianceM2(double *y, int *N, int J, int I, double alpha, double beta, int iterations, double *chains, double *debug, double sdMetropg, double sdDecorr, int newtonSteps, int progress, SEXP pBar, SEXP rho);
-
-void decorrStepEqVarM2(double sumg, double sig2g, int J, double *g, double *sig2, double sdDecorr, double *acc);
-
-double newtonMethodEqVar2(double xt, double c1, double c2, double c3, int iterations, int max);
-
-double samplegEqVarM2(double g, double sig2, double mu, double sig2g, double yBar, double sumy2, int N, double sdMetrop, double *acc);
-
-double fullCondgEqVarM2(double g, double sig2, double mu, double sig2g, double yBar, double sumy2, int N);
-
-double thetaLogLikeAR(double theta, double mu, double delta, double sig2, double g, double *y, int N, double *t, double alphaTheta, double betaTheta);
-
-double sampThetaAR(double theta, double mu, double delta, double sig2, double g, double *y, int N, double *t, double alphaTheta, double betaTheta , double sdmet);
-
-void gibbsTwoSampleAR(double *y, int N, double *t, double rscale, double alphaTheta, double betaTheta, double loArea, double upArea, int iterations, double sdmet, double *chains, int progress, SEXP pBar, SEXP rho);
-
-SEXP RgibbsTwoSampleAR(SEXP yR, SEXP NR, SEXP tR, SEXP rscaleR, SEXP alphaThetaR, SEXP betaThetaR, SEXP loArea, SEXP upArea, SEXP iterationsR, SEXP sdmet, SEXP progressR, SEXP pBar, SEXP rho);
-
-double MCTwoSampleAR(double *y, int N, double *t, double rscale, double alphaTheta, double betaTheta, int iterations);
-
-SEXP RMCTwoSampleAR(SEXP yR, SEXP NR, SEXP tR, SEXP rscaleR, SEXP alphaThetaR, SEXP betaThetaR, SEXP iterationsR);
-
 
 SEXP RjeffSamplerNwayAov(SEXP Riters, SEXP RXtCX, SEXP RXtCy, SEXP RytCy, SEXP RN, SEXP RP, SEXP RnGs, SEXP RgMap, SEXP Ra, SEXP Rb, SEXP progressR, SEXP pBar, SEXP rho);
 
@@ -87,20 +56,13 @@ void debugPrintMatrix(double *X, int rows, int cols);
 
 void debugPrintVector(double *x, int len);
 
-SEXP RgibbsTwoSampleARmulti(SEXP yR, SEXP NR, SEXP NobsR, SEXP tR, SEXP rscaleR, SEXP betaThetaR, SEXP iterationsR, SEXP sdmetR, SEXP progressR, SEXP pBar, SEXP rho);
-
-void gibbsTwoSampleARmulti(double *y, int N, int *Nobs, double *t, double rscale, double betaTheta, int iterations, double sdmet, double *chains, int progress, SEXP pBar, SEXP rho);
-
-double sampThetaARmulti(double theta, double *mu0, double *beta, double *sig2, double g, double *y, int N, double *t, int *Nobs, int *cumobs, int maxobs, double betaTheta , double sdmet);
-
-double thetaLogLikeARmulti(double theta, double *mu0, double *beta, double *sig2, double g, double *y, int N, double *t, int *Nobs, int *cumobs, int maxobs, double betaTheta);
-
-SEXP RthetaLogLikeARmulti(SEXP thetaR, SEXP mu0R, SEXP betaR, SEXP sig2R, SEXP gR, SEXP yR, SEXP NR, SEXP tR, SEXP NobsR, SEXP cumobsR, SEXP maxobsR, SEXP betaThetaR);
-
 void GibbsNwayAov(double *chains, int iters, double *y, double *X, double *XtX, double *Xty, int N, int P, int nGs, int *gMap, double *r, int progress, SEXP pBar, SEXP rho);
 
 SEXP RGibbsNwayAov(SEXP Riters, SEXP Ry, SEXP RX, SEXP RXtX, SEXP RXty, SEXP RN, SEXP RP, SEXP RnGs, SEXP RgMap, SEXP Rr, SEXP progressR, SEXP pBar, SEXP rho);
 
+SEXP RGibbsLinearReg(SEXP Riters, SEXP RCny, SEXP RX, SEXP RXtX, SEXP RXtCnX, SEXP RXtCny, SEXP RN, SEXP RP, SEXP Rr, SEXP sig2start, SEXP progressR, SEXP pBar, SEXP rho);
+
+void GibbsLinearReg(double *chains, int iters, double *Cny, double *X, double *XtX, double *XtCnX, double *XtCny, int N, int P, double r, double sig2start, int progress, SEXP pBar, SEXP rho);
 
 
 #define AZERO(x, n) {int _I_, _SZ_ = (n); for(_I_ = 0; _I_ < _SZ_; _I_++) (x)[_I_] = 0;}
