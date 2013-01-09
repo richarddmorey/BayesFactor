87a2f8b68d7a466756364afb6887e21bd92bc081
Richard D. Morey <richarddmorey@gmail.com>
2013-01-09 19:41:27 +0000

    added my email to the startup message, in case people have problems.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesfactorpcl@244 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BayesFactorPCL/R/onAttach.R
+++ pkg/BayesFactorPCL/R/onAttach.R
@@ -1,6 +1,7 @@
 .onAttach<- function(libname, pkgname){
   packageStartupMessage("************\nWelcome to ",pkgname," ",BFInfo(FALSE),". Much has changed since the ",
                         "last version, including new functionality and function name changes. ",
-                        "Please see the help files for the new function names and arguments.\n************", 
+                        "Please see the help files for the new function names and arguments. If you have",
+                        " questions, please contact Richard Morey (richarddmorey@gmail.com).\n************", 
                         appendLF = TRUE)
 }
