899971b4f3662aa51fce1ad9382499824ddd584c
Richard D. Morey <richarddmorey@gmail.com>
2013-01-15 19:34:32 +0000

    Added BFManual() function to open the HTML manual.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesfactorpcl@257 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BayesFactorPCL/R/onAttach.R
+++ pkg/BayesFactorPCL/R/onAttach.R
@@ -2,6 +2,7 @@
   packageStartupMessage("************\nWelcome to ",pkgname," ",BFInfo(FALSE),". Much has changed since the ",
                         "last version, including new functionality and function name changes. ",
                         "Please see the help files for the new function names and arguments. If you have",
-                        " questions, please contact Richard Morey (richarddmorey@gmail.com).\n************", 
+                        " questions, please contact Richard Morey (richarddmorey@gmail.com).\n\n",
+                        "Type BFManual() to open the manual.\n************", 
                         appendLF = TRUE)
 }
