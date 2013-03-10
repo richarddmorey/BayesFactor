1c72b79b0c259df5376e82cc33bddccd1f6ac969
Richard D. Morey <richarddmorey@gmail.com>
2013-03-10 19:56:41 +0000

    Changed .onAttach message.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesfactorpcl@272 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BayesFactorPCL/R/onAttach.R
+++ pkg/BayesFactorPCL/R/onAttach.R
@@ -1,7 +1,5 @@
 .onAttach<- function(libname, pkgname){
-  packageStartupMessage("************\nWelcome to ",pkgname," ",BFInfo(FALSE),". Much has changed since the ",
-                        "last version, including new functionality and function name changes. ",
-                        "Please see the help files for the new function names and arguments. If you have",
+  packageStartupMessage("************\nWelcome to ",pkgname," ",BFInfo(FALSE),". If you have",
                         " questions, please contact Richard Morey (richarddmorey@gmail.com).\n\n",
                         "Type BFManual() to open the manual.\n************", 
                         appendLF = TRUE)
