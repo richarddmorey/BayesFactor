77666c3c57eadf82a434bc653c010fa79f4a68e4
Richard D. Morey <richarddmorey@gmail.com>
2013-03-25 23:00:23 +0000

    Added help for BayesFactor-specific options; corrected typo in manual
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesfactorpcl@300 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BayesFactorPCL/R/onAttach.R
+++ pkg/BayesFactorPCL/R/onAttach.R
@@ -6,6 +6,31 @@
   setOptions()
 }
 
+#'options() for package BayesFactor
+#'
+#'Options that can be set for the BayesFactor package
+#'
+#'The BayesFactor package has numerous options that can be set to globally 
+#'change the behavior of the functions in the package. These options can be
+#'changed using \code{\link[base]{options}}().
+#'
+#'\describe{
+#'\item{\code{BFMaxModels}}{Integer; maximum number of models to analyze in \code{\link{anovaBF}} or \code{\link{regressionBF}}} 
+#'\item{\code{BFprogress}}{If \code{TRUE}, progress bars are on by default; if \code{FALSE}, they are disabled by default.}
+#'\item{\code{BFpretestIterations}}{Integer; if sampling is needed to compute the Bayes factor, the package attempts to 
+#'choose the most efficient sampler. This option controls the number of initial test iterations.}
+#'\item{\code{BFapproxOptimizer}}{\code{"nlm"} or \code{"optim"}; changes the optimization function used for the importance sampler. If one fails, try the other.}
+#'\item{\code{BFapproxLimits}}{Vector of length two containing the lower and upper limits on 
+#'on \code{log(g)} before the the posterior returns \code{-Inf}. This only affects the initial optimization step for the importance sampler.}
+#'}
+#'
+#'@name options-BayesFactor
+#'@seealso \code{\link[base]{options}}
+
+NULL
+
+
+
 setOptions <- function(){
   
   if(is.null(options()$BFMaxModels)) options(BFMaxModels = 50000)
