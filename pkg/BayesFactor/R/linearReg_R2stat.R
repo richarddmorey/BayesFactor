768ed14298d3e178beddef49bb57cea11cd8ae8f
richarddmorey <richarddmorey@gmail.com>
2016-01-03 19:15:19 +0000

    corrected outdated documentation and typo

--- pkg/BayesFactor/R/linearReg_R2stat.R
+++ pkg/BayesFactor/R/linearReg_R2stat.R
@@ -50,7 +50,7 @@
 ##' summary(lm1)
 ##' 
 ##' ## Bayes factor
-##' ## The Bayes factor is almost 80,000;
+##' ## The Bayes factor is over 400,000;
 ##' ## the data strongly favor hypothesis that 
 ##' ## the slope is not 0.
 ##' result = linearReg.R2stat(30,1,0.6813)
