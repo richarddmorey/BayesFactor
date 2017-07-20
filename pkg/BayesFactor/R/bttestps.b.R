c1e4e0afe2c0a614c7704baa175abaa4016544f2
Jonathon Love <jon@thon.cc>
2017-07-20 12:43:24 +0100

    Added jmvcore t-tests

---
+++ pkg/BayesFactor/R/bttestps.b.R
@@ -0,0 +1,20 @@
+
+bttestPSClass <- if (requireNamespace('jmvcore')) R6::R6Class(
+  "bttestPSClass",
+  inherit = bttestPSBase,
+  private = list(
+    .init = function() {
+
+      ci <- paste0(self$options$ciWidth, '% Credible Interval')
+      self$results$ttest$getColumn('cil')$setSuperTitle(ci)
+      self$results$ttest$getColumn('ciu')$setSuperTitle(ci)
+
+    },
+    .run = function() {
+
+      # `self$data` contains the data
+      # `self$options` contains the options
+      # `self$results` contains the results object (to populate)
+
+    })
+)
