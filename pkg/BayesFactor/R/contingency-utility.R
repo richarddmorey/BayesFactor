bcb2f9e606265cb8d127419c7df568ff98d2946a
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-06-16 15:21:04 +0200

    Remove reliance on `{gtools}` package
    Closes #161

--- pkg/BayesFactor/R/contingency-utility.R
+++ pkg/BayesFactor/R/contingency-utility.R
@@ -428,4 +428,9 @@ sampleHypergeomContingencyAlt <- function(prior, data, iterations, noSample = FA
 
 }
 
-
+rdirichlet <- function(n, alpha) {
+  l <- length(alpha)
+  x <- matrix(stats::rgamma(l * n, alpha), ncol = l, byrow = TRUE)
+  sm <- x %*% rep(1, l)
+  x / as.vector(sm)
+}
