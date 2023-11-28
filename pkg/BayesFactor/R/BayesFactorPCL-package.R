14921a305da16852aed79694e0c37fdf81f791a2
Richard Morey <richarddmorey@gmail.com>
2023-11-28 20:40:53 +0000

    fixed issue with REprintf format

--- pkg/BayesFactor/R/BayesFactorPCL-package.R
+++ pkg/BayesFactor/R/BayesFactorPCL-package.R
@@ -8,7 +8,7 @@
 #'regression, correlations, proportions, and contingency tables.
 #'
 #'\tabular{ll}{ Package: \tab BayesFactor\cr Type: \tab Package\cr Version: \tab
-#'0.9.12-4.5\cr Date: \tab 2023-09-21\cr License: \tab GPL 2.0\cr LazyLoad: \tab
+#'0.9.12-4.6\cr Date: \tab 2023-11-28\cr License: \tab GPL 2.0\cr LazyLoad: \tab
 #'yes\cr } The following methods are currently implemented, with more to follow:
 #'
 #'general linear models (including linear mixed effects models): \code{\link{generalTestBF}}, \code{\link{lmBF}}
