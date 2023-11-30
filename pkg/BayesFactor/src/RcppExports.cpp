04b83d90d414c0df1b29136fa76921af2cfeca9b
Richard Morey <richarddmorey@gmail.com>
2023-11-30 18:53:55 +0000

    Update RcppExports.cpp

--- pkg/BayesFactor/src/RcppExports.cpp
+++ pkg/BayesFactor/src/RcppExports.cpp
@@ -153,7 +153,7 @@ RcppExport SEXP _BayesFactor_genhypergeo_series_pos(SEXP USEXP, SEXP LSEXP, SEXP
     if (rcpp_isError_gen) {
         SEXP rcpp_msgSEXP_gen = Rf_asChar(rcpp_result_gen);
         UNPROTECT(1);
-        Rf_error(CHAR(rcpp_msgSEXP_gen));
+        Rf_error("%s", CHAR(rcpp_msgSEXP_gen));
     }
     UNPROTECT(1);
     return rcpp_result_gen;
